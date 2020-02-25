7f8615ca6046df2fa5178086ca5cb472c8463569
Thomas Darimont <thomas.darimont@googlemail.com>
2020-02-25 22:30:48 +0100

    Add example for remote oidc claim mapper

--- adhoc-keycloak-extensions/src/main/java/demo/keycloak/oidcmappers/RemoteOidcMapper.java
+++ adhoc-keycloak-extensions/src/main/java/demo/keycloak/oidcmappers/RemoteOidcMapper.java
@@ -1,11 +1,15 @@
-package com.github.thomasdarimont.keycloak.oidc.mapper.simple;
+package demo.keycloak.oidcmappers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.auto.service.AutoService;
 import org.jboss.logging.Logger;
+import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserModel;
 import org.keycloak.models.UserSessionModel;
-import org.keycloak.protocol.ProtocolMapperUtils;
+import org.keycloak.protocol.ProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
@@ -16,37 +20,31 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.representations.IDToken;
 
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
-public class SimpleOidcMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+@AutoService(ProtocolMapper.class)
+public class RemoteOidcMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
 
-    private static final String PROVIDER_ID = "oidc-simple-protocol-mapper";
+    private static final String PROVIDER_ID = "oidc-remote-protocol-mapper";
 
-    private static final Logger LOGGER = Logger.getLogger(SimpleOidcMapper.class);
+    private static final Logger LOGGER = Logger.getLogger(RemoteOidcMapper.class);
 
     private static final List<ProviderConfigProperty> CONFIG_PROPERTIES;
 
-    private static final String CONFIG_PROPERTY = "configProperty";
+    private static final String REMOTE_URL_PROPERTY = "remoteUrl";
 
     static {
 
         CONFIG_PROPERTIES = ProviderConfigurationBuilder.create()
                 .property()
-                .name(CONFIG_PROPERTY)
+                .name(REMOTE_URL_PROPERTY)
                 .type(ProviderConfigProperty.STRING_TYPE)
-                .label("Config value")
-                .helpText("A description for the config value")
-                .defaultValue("defaultValue")
+                .label("Remote URL")
+                .helpText("URL to fetch custom claims for the given user")
+                //.defaultValue("http://localhost:")
                 .add()
-
-                .property()
-                .name(ProtocolMapperUtils.MULTIVALUED)
-                .label(ProtocolMapperUtils.MULTIVALUED_LABEL)
-                .helpText(ProtocolMapperUtils.MULTIVALUED_HELP_TEXT)
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .defaultValue(false)
-                .add()
-
                 .build();
 
         OIDCAttributeMapperHelper.addAttributeConfig(CONFIG_PROPERTIES, UserPropertyMapper.class);
@@ -59,12 +57,12 @@ public class SimpleOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
 
     @Override
     public String getDisplayType() {
-        return "Demo Simple Mapper";
+        return "Demo Remote Mapper";
     }
 
     @Override
     public String getHelpText() {
-        return "A simple oidc token mapper";
+        return "A protocol mapper that can fetch additional claims from an external service";
     }
 
     @Override
@@ -80,9 +78,25 @@ public class SimpleOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
     @Override
     protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
 
-        Object claimValue = mappingModel.getConfig().getOrDefault(CONFIG_PROPERTY, "defaultProperty");
+        Object claimValue = fetchRemoteClaims(mappingModel, userSession, keycloakSession);
+
         LOGGER.infof("setClaim %s=%s", mappingModel.getName(), claimValue);
 
         OIDCAttributeMapperHelper.mapClaim(token, mappingModel, claimValue);
+    }
+
+    private Object fetchRemoteClaims(ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession) {
+
+        try {
+            String remoteUrl = mappingModel.getConfig().getOrDefault(REMOTE_URL_PROPERTY, "http://localhost:7777/claims");
+            UserModel user = userSession.getUser();
+            String url = remoteUrl + "?userId=" + user.getId() + "&username=" + URLEncoder.encode(user.getUsername(), "UTF-8");
+            JsonNode jsonNode = SimpleHttp.doGet(url, keycloakSession).asJson();
+            return jsonNode;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
