61853379a65317f7b5e99e0300c4e0d621bcc011
Thomas Darimont <thomas.darimont@googlemail.com>
2020-07-22 15:56:27 +0200

    Add dummy adhoc authenticator

--- adhoc-keycloak-extensions/src/main/java/demo/keycloak/auth/AdHocAuthenticator.java
+++ adhoc-keycloak-extensions/src/main/java/demo/keycloak/auth/AdHocAuthenticator.java
@@ -1,18 +1,30 @@
 package demo.keycloak.auth;
 
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.spi.HttpRequest;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
+import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.FormMessage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.Response;
+import java.util.Map;
 
 public class AdHocAuthenticator implements Authenticator {
 
     private static final Logger LOG = Logger.getLogger(AdHocAuthenticator.class);
+    private final KeycloakSession session;
 
     public AdHocAuthenticator(KeycloakSession session) {
-        // configure from session
+        this.session = session;
     }
 
     @Override
@@ -26,7 +38,30 @@ public class AdHocAuthenticator implements Authenticator {
             LOG.infof("Pass through: %s%n", "anonymous");
         }
 
-       context.success();
+//        context.form().setError("custom_error");
+//        context.failure(AuthenticationFlowError.INTERNAL_ERROR);
+
+//        HttpRequest httpRequest = context.getHttpRequest();
+//
+//        boolean shouldWait = false;
+//        if (httpRequest.getUri().getQueryParameters().containsKey("wait")) {
+//            HttpServletResponse httpResponse = ResteasyProviderFactory.getContextData(HttpServletResponse.class);
+//            javax.servlet.http.Cookie cookie = new javax.servlet.http.Cookie("keycloak-pacer", "1");
+//            cookie.setMaxAge((int)(System.currentTimeMillis() / 1000) + 5); // 5 second wait time
+//            httpResponse.addCookie(cookie);
+//            shouldWait = true;
+//        }
+//
+//        Map<String, Cookie> cookies = httpRequest.getHttpHeaders().getCookies();
+//        Cookie cookie = cookies.get("keycloak-pacer");
+//        if (cookie != null || shouldWait) {
+//            LoginFormsProvider form = context.form().setExecution(context.getExecution().getId());
+//            form.setError("please-wait");
+//            Response response = form.createLoginUsernamePassword();
+//            context.failureChallenge(AuthenticationFlowError.USER_TEMPORARILY_DISABLED, response);
+//        }
+
+        context.success();
     }
 
     @Override
