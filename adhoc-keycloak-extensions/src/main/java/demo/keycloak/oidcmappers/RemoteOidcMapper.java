9ef4b54cb33b4d817e415e48af4acf08260e2660
Thomas Darimont <thomas.darimont@googlemail.com>
2020-07-22 15:52:53 +0200

    Revise RemoteOidcMapper

--- adhoc-keycloak-extensions/src/main/java/demo/keycloak/oidcmappers/RemoteOidcMapper.java
+++ adhoc-keycloak-extensions/src/main/java/demo/keycloak/oidcmappers/RemoteOidcMapper.java
@@ -1,7 +1,7 @@
 package demo.keycloak.oidcmappers;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.auto.service.AutoService;
+import lombok.extern.jbosslog.JBossLog;
 import org.jboss.logging.Logger;
 import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.models.ClientSessionContext;
@@ -20,10 +20,31 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.representations.IDToken;
 
+import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.util.List;
 
+/**
+ * <pre>{@code
+ *
+ * KC_ISSUER=http://localhost:8081/auth/realms/remote-claims
+ * KC_CLIENT_ID=demo-client-remote-claims
+ * KC_USERNAME=tester
+ * KC_PASSWORD=test
+ *
+ * KC_RESPONSE=$( \
+ * curl \
+ *   -d "client_id=$KC_CLIENT_ID" \
+ *   -d "username=$KC_USERNAME" \
+ *   -d "password=$KC_PASSWORD" \
+ *   -d "grant_type=password" \
+ *   "$KC_ISSUER/protocol/openid-connect/token" \
+ * )
+ * echo $KC_RESPONSE | jq -C .
+ *
+ * }</pre>
+ */
+@JBossLog
 @AutoService(ProtocolMapper.class)
 public class RemoteOidcMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
 
@@ -35,6 +56,8 @@ public class RemoteOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
 
     private static final String REMOTE_URL_PROPERTY = "remoteUrl";
 
+    public static final String DEFAULT_REMOTE_CLAIM_URL = "http://localhost:7777/claims?userId={userId}&username={username}&clientId={clientId}&issuer={issuer}";
+
     static {
 
         CONFIG_PROPERTIES = ProviderConfigurationBuilder.create()
@@ -43,7 +66,7 @@ public class RemoteOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
                 .type(ProviderConfigProperty.STRING_TYPE)
                 .label("Remote URL")
                 .helpText("URL to fetch custom claims for the given user")
-                //.defaultValue("http://localhost:")
+                .defaultValue(DEFAULT_REMOTE_CLAIM_URL)
                 .add()
                 .build();
 
@@ -78,23 +101,22 @@ public class RemoteOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
     @Override
     protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
 
-        Object claimValue = fetchRemoteClaims(mappingModel, userSession, keycloakSession);
+        Object claimValue = fetchRemoteClaims(mappingModel, userSession, keycloakSession, token.getIssuer(), token.getIssuedFor());
 
         LOGGER.infof("setClaim %s=%s", mappingModel.getName(), claimValue);
 
         OIDCAttributeMapperHelper.mapClaim(token, mappingModel, claimValue);
     }
 
-    private Object fetchRemoteClaims(ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession) {
+    private Object fetchRemoteClaims(ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, String issuer, String clientId) {
 
         try {
-            String remoteUrl = mappingModel.getConfig().getOrDefault(REMOTE_URL_PROPERTY, "http://localhost:7777/claims");
+            String remoteUrlTemplate = mappingModel.getConfig().getOrDefault(REMOTE_URL_PROPERTY, DEFAULT_REMOTE_CLAIM_URL);
             UserModel user = userSession.getUser();
-            String url = remoteUrl + "?userId=" + user.getId() + "&username=" + URLEncoder.encode(user.getUsername(), "UTF-8");
-            JsonNode jsonNode = SimpleHttp.doGet(url, keycloakSession).asJson();
-            return jsonNode;
+            String url = UriBuilder.fromUri(remoteUrlTemplate).build(user.getId(), user.getUsername(), clientId, issuer).toString();
+            return SimpleHttp.doGet(url, keycloakSession).asJson();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.warn("Could not fetch remote claims for user", e);
         }
 
         return null;
