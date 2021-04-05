f0a541c76d02b15e84dd3af47d57c6397428386f
Thomas Darimont <thomas.darimont@gmail.com>
2021-04-05 13:48:25 +0200

    Add example for texting custom Keycloak extensions

--- simple-oidc-mapper/src/main/java/com/github/thomasdarimont/keycloak/oidc/mapper/simple/SimpleOidcMapper.java
+++ simple-oidc-mapper/src/main/java/com/github/thomasdarimont/keycloak/oidc/mapper/simple/SimpleOidcMapper.java
@@ -1,60 +1,44 @@
 package com.github.thomasdarimont.keycloak.oidc.mapper.simple;
 
-import org.jboss.logging.Logger;
+import com.google.auto.service.AutoService;
+import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
 import org.keycloak.models.UserSessionModel;
-import org.keycloak.protocol.ProtocolMapperUtils;
+import org.keycloak.protocol.ProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
 import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
-import org.keycloak.protocol.oidc.mappers.UserPropertyMapper;
 import org.keycloak.provider.ProviderConfigProperty;
-import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.representations.IDToken;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@JBossLog
+@AutoService(ProtocolMapper.class)
 public class SimpleOidcMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
 
-    private static final String PROVIDER_ID = "oidc-simple-protocol-mapper";
+    static final String PROVIDER_ID = "oidc-simple-protocol-mapper";
 
-    private static final Logger LOGGER = Logger.getLogger(SimpleOidcMapper.class);
+    static final String CLAIM_NAME = "customClaim";
 
-    private static final List<ProviderConfigProperty> CONFIG_PROPERTIES;
-
-    private static final String CONFIG_PROPERTY = "configProperty";
+    static final List<ProviderConfigProperty> CONFIG_PROPERTIES;
 
     static {
 
-        CONFIG_PROPERTIES = ProviderConfigurationBuilder.create()
-                .property()
-                .name(CONFIG_PROPERTY)
-                .type(ProviderConfigProperty.STRING_TYPE)
-                .label("Config value")
-                .helpText("A description for the config value")
-                .defaultValue("defaultValue")
-                .add()
+        List<ProviderConfigProperty> configProperties = new ArrayList<>();
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, SimpleOidcMapper.class);
 
-                .property()
-                .name(ProtocolMapperUtils.MULTIVALUED)
-                .label(ProtocolMapperUtils.MULTIVALUED_LABEL)
-                .helpText(ProtocolMapperUtils.MULTIVALUED_HELP_TEXT)
-                .type(ProviderConfigProperty.BOOLEAN_TYPE)
-                .defaultValue(false)
-                .add()
-
-                .build();
-
-        OIDCAttributeMapperHelper.addAttributeConfig(CONFIG_PROPERTIES, UserPropertyMapper.class);
+        CONFIG_PROPERTIES = configProperties;
     }
 
     @Override
-    public String getDisplayCategory() {
-        return TOKEN_MAPPER_CATEGORY;
+    public String getId() {
+        return PROVIDER_ID;
     }
 
     @Override
@@ -68,21 +52,21 @@ public class SimpleOidcMapper extends AbstractOIDCProtocolMapper implements OIDC
     }
 
     @Override
+    public String getDisplayCategory() {
+        return TOKEN_MAPPER_CATEGORY;
+    }
+
+    @Override
     public List<ProviderConfigProperty> getConfigProperties() {
         return CONFIG_PROPERTIES;
     }
 
     @Override
-    public String getId() {
-        return PROVIDER_ID;
-    }
-
-    @Override
     protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession, ClientSessionContext clientSessionCtx) {
 
-        Object claimValue = mappingModel.getConfig().getOrDefault(CONFIG_PROPERTY, "defaultProperty");
-        LOGGER.infof("setClaim %s=%s", mappingModel.getName(), claimValue);
+        Object claimValue = "testdata:" + System.currentTimeMillis();
+        log.infof("setClaim %s=%s", CLAIM_NAME, claimValue);
 
-        OIDCAttributeMapperHelper.mapClaim(token, mappingModel, claimValue);
+        token.getOtherClaims().put(CLAIM_NAME, claimValue);
     }
 }
---
+++ simple-oidc-mapper/src/test/java/com/github/thomasdarimont/keycloak/oidc/mapper/simple/SimpleOidcMapperTest.java
@@ -0,0 +1,81 @@
+package com.github.thomasdarimont.keycloak.oidc.mapper.simple;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.Test;
+import org.keycloak.TokenVerifier;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.AccessTokenResponse;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.ProtocolMapperRepresentation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+
+public class SimpleOidcMapperTest {
+
+    public static final String MASTER_REALM = "master";
+
+    public static final String ADMIN_CLI = "admin-cli";
+
+    /**
+     * Deploys the Keycloak extensions from the classes folder into the create Keycloak container.
+     */
+    @Test
+    public void shouldDeployExtension() throws Exception {
+
+        try (KeycloakContainer keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:12.0.4")
+                .withRealmImportFile("test-realm.json")
+                .withExtensionClassesFrom("target/classes")
+        ) {
+            keycloak.start();
+
+            Keycloak keycloakClient = Keycloak.getInstance(
+                    keycloak.getAuthServerUrl(), MASTER_REALM,
+                    keycloak.getAdminUsername(), keycloak.getAdminPassword(), ADMIN_CLI);
+
+            RealmResource realm = keycloakClient.realm(MASTER_REALM);
+            ClientRepresentation client = realm.clients().findByClientId(ADMIN_CLI).get(0);
+
+            configureCustomOidcProtocolMapper(realm, client);
+
+            keycloakClient.tokenManager().refreshToken();
+            AccessTokenResponse tokenResponse = keycloakClient.tokenManager().getAccessToken();
+
+            // parse the received access-token
+            TokenVerifier<AccessToken> verifier = TokenVerifier.create(tokenResponse.getToken(), AccessToken.class);
+            verifier.parse();
+
+            // check for the custom claim
+            AccessToken accessToken = verifier.getToken();
+            String customClaimValue = (String) accessToken.getOtherClaims().get(SimpleOidcMapper.CLAIM_NAME);
+            System.out.printf("Custom Claim name %s=%s", SimpleOidcMapper.CLAIM_NAME, customClaimValue);
+            assertThat(customClaimValue, notNullValue());
+            assertThat(customClaimValue, startsWith("testdata:"));
+        }
+    }
+
+    /**
+     * Configures the {@link SimpleOidcMapper} to the given client in the given realm.
+     */
+    private static void configureCustomOidcProtocolMapper(RealmResource realm, ClientRepresentation client) {
+
+        ProtocolMapperRepresentation mapper = new ProtocolMapperRepresentation();
+        mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
+        mapper.setProtocolMapper(SimpleOidcMapper.PROVIDER_ID);
+        mapper.setName("test-simple-oidc-mapper");
+
+        Map<String, String> config = new HashMap<>();
+        config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
+        mapper.setConfig(config);
+
+        realm.clients().get(client.getId()).getProtocolMappers().createMapper(mapper).close();
+    }
+}
