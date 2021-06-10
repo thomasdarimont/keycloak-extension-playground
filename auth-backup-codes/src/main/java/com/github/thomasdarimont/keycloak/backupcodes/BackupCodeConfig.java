6bacd3631150918f68375aba3a6673d8684feb0e
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-10 11:37:03 +0200

    use PBKDF2-sha256 for backup code hashes

--- auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeConfig.java
+++ auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeConfig.java
@@ -2,6 +2,7 @@ package com.github.thomasdarimont.keycloak.backupcodes;
 
 import lombok.Data;
 import org.keycloak.credential.hash.Pbkdf2PasswordHashProviderFactory;
+import org.keycloak.credential.hash.Pbkdf2Sha256PasswordHashProviderFactory;
 import org.keycloak.models.RealmModel;
 
 @Data
@@ -10,8 +11,8 @@ public class BackupCodeConfig {
     private int backupCodeCount = Integer.getInteger("keycloak_backup_code_count", 10);
     private int backupCodeLength = Integer.getInteger("keycloak_backup_code_length", 8);
 
-    private String hashingProviderId = System.getProperty("keycloak_backup_code_hash_provider", Pbkdf2PasswordHashProviderFactory.ID);
-    private int backupCodeHashIterations = Integer.getInteger("keycloak_backup_code_hash_iterations", 100);
+    private String hashingProviderId = System.getProperty("keycloak_backup_code_hash_provider", Pbkdf2Sha256PasswordHashProviderFactory.ID);
+    private int backupCodeHashIterations = Integer.getInteger("keycloak_backup_code_hash_iterations", 1000);
 
     private static final BackupCodeConfig DEFAULT = new BackupCodeConfig();
 
