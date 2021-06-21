7e87781d7469857f4a73a1e03dd2dabdf18f0e33
Thomas Darimont <thomas.darimont@googlemail.com>
2021-06-21 10:01:31 +0200

    Revise BackupCodeConfig

--- auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeConfig.java
+++ auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeConfig.java
@@ -1,63 +1,34 @@
 package com.github.thomasdarimont.keycloak.backupcodes;
 
+import lombok.Data;
 import org.keycloak.credential.hash.Pbkdf2Sha256PasswordHashProviderFactory;
 import org.keycloak.models.RealmModel;
 
+@Data
 public class BackupCodeConfig {
 
-    // TODO how to store backup code configuration? can we use password policies for this, or realm attributes?
+    private int backupCodeCount;
+    private int backupCodeLength;
 
-    private int backupCodeCount = Integer.getInteger("keycloak_backup_code_count", 10);
-    private int backupCodeLength = Integer.getInteger("keycloak_backup_code_length", 8);
+    private String hashingProviderId;
+    private int backupCodeHashIterations;
 
-    private String hashingProviderId = System.getProperty("keycloak_backup_code_hash_provider", Pbkdf2Sha256PasswordHashProviderFactory.ID);
-    private int backupCodeHashIterations = Integer.getInteger("keycloak_backup_code_hash_iterations", 1000);
+    private static final BackupCodeConfig DEFAULT;
 
-    private static final BackupCodeConfig DEFAULT = new BackupCodeConfig();
+    static {
+
+        BackupCodeConfig config = new BackupCodeConfig();
+        config.setBackupCodeCount(Integer.getInteger("keycloak_backup_code_count", 10));
+        config.setBackupCodeLength(Integer.getInteger("keycloak_backup_code_length", 8));
+        config.setHashingProviderId(System.getProperty("keycloak_backup_code_hash_provider", Pbkdf2Sha256PasswordHashProviderFactory.ID));
+        config.setBackupCodeHashIterations(Integer.getInteger("keycloak_backup_code_hash_iterations", 1000));
+
+        DEFAULT = config;
+    }
 
     public static BackupCodeConfig getConfig(RealmModel realmModel) {
+        // TODO make backup-code handling configurable via realm
         return DEFAULT;
     }
 
-    public int getBackupCodeCount() {
-        return backupCodeCount;
-    }
-
-    public void setBackupCodeCount(int backupCodeCount) {
-        this.backupCodeCount = backupCodeCount;
-    }
-
-    public int getBackupCodeLength() {
-        return backupCodeLength;
-    }
-
-    public void setBackupCodeLength(int backupCodeLength) {
-        this.backupCodeLength = backupCodeLength;
-    }
-
-    public String getHashingProviderId() {
-        return hashingProviderId;
-    }
-
-    public void setHashingProviderId(String hashingProviderId) {
-        this.hashingProviderId = hashingProviderId;
-    }
-
-    public int getBackupCodeHashIterations() {
-        return backupCodeHashIterations;
-    }
-
-    public void setBackupCodeHashIterations(int backupCodeHashIterations) {
-        this.backupCodeHashIterations = backupCodeHashIterations;
-    }
-
-    @Override
-    public String toString() {
-        return "BackupCodeConfig{" +
-                "backupCodeCount=" + backupCodeCount +
-                ", backupCodeLength=" + backupCodeLength +
-                ", hashingProviderId='" + hashingProviderId + '\'' +
-                ", backupCodeHashIterations=" + backupCodeHashIterations +
-                '}';
-    }
 }
