f406f9e0e3368baeb48d7979e65952bb46d2dbd3
Lucas Hedding <heddn@users.noreply.github.com>
2021-12-21 15:51:43 -0600

    Resolve deprecation in calling RandomString
    Instead call SecretGenerator

--- auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeGenerator.java
+++ auth-backup-codes/src/main/java/com/github/thomasdarimont/keycloak/backupcodes/BackupCodeGenerator.java
@@ -1,12 +1,12 @@
 package com.github.thomasdarimont.keycloak.backupcodes;
 
-import org.keycloak.common.util.RandomString;
+import org.keycloak.common.util.SecretGenerator;
 
 public class BackupCodeGenerator {
 
     // TODO consider using a method like Base32 to reduce potential for typos when a code is entered manually
 
     public static String generateCode(int length) {
-        return RandomString.randomCode(length);
+        return SecretGenerator.getInstance().randomString(length);
     }
 }
